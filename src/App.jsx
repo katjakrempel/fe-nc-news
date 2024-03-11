ced47a00ee51eba8e2d36a3e60e9265f48f5a9b1
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-03-11 17:58:48 +0000

    add Home and TopicList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import ArticleList from "./components/ArticleList";
+import TopicList from "./components/TopicList";
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/articles" element={<ArticleList />} />
+        <Route path="/topics" element={<TopicList />} />
       </Routes>
     </div>
   );
