39e7849a9e852191ef5af445d94c2a651333768c
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-03-12 12:16:26 +0000

    add date to article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,7 +2,9 @@ function ArticleCard({ article }) {
   return (
     <li className="article-card">
       <h3>{article.title}</h3>
-      <p>Posted by: {article.author}</p>
+      <p>
+        Posted by {article.author} at {article.created_at.slice(0, 10)}
+      </p>
       <img src={article.article_img_url} />
       <p>Topic: {article.topic}</p>
       <p>Comments: {article.comment_count}</p>
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -5,17 +5,21 @@ import { fetchArticleById } from "../../utils/api";
 function SingleArticle() {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     fetchArticleById(article_id).then((response) => {
       setArticle(response);
+      setIsLoading(false);
     });
   }, [article_id]);
 
+  if (isLoading) return <p>Loading...</p>;
   return (
     <div className="single-article">
       <h3>{article.title}</h3>
-      <p>Posted by: {article.author}</p>
+      <p>Posted by {article.author} at {article.created_at.slice(0,10)}</p>
       <img src={article.article_img_url} />
       <p>Topic: {article.topic}</p>
       <p>Comments: {article.comment_count}</p>
