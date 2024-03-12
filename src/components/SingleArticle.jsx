14da364e74030e6ccb89e8a75c35b9868f60a7a7
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-03-12 15:16:26 +0000

    add CommentCard with static data

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,10 +1,29 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchArticleById } from "../../utils/api";
+import CommentCard from "./CommentCard";
 
 function SingleArticle() {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
+  const [comments, setComments] = useState([
+    {
+      comment_id: 16,
+      body: "This is a bad article name",
+      article_id: 6,
+      author: "butter_bridge",
+      votes: 1,
+      created_at: "2020-10-11T15:23:00.000Z",
+    },
+    {
+      comment_id: 167,
+      body: "Deleniti itaque et est unde autem. Labore illo commodi quaerat natus fugiat adipisci. Adipisci unde recusandae aliquam suscipit ipsum.",
+      article_id: 3,
+      author: "grumpy19",
+      votes: 19,
+      created_at: "2020-02-05T09:16:00.000Z",
+    },
+  ]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -17,16 +36,26 @@ function SingleArticle() {
 
   if (isLoading) return <p>Loading...</p>;
   return (
-    <div className="single-article">
-      <h3>{article.title}</h3>
-      <p>
-        Posted by {article.author} at {article.created_at.slice(0, 10)}
-      </p>
-      <img src={article.article_img_url} />
-      <p>Topic: {article.topic}</p>
-      <p>Comments: {article.comment_count}</p>
-      <p>Votes: {article.votes}</p>
-      <p>{article.body}</p>
+    <div>
+      <section className="single-article">
+        <h3>{article.title}</h3>
+        <p>
+          Posted by {article.author} at {article.created_at.slice(0, 10)}
+        </p>
+        <img src={article.article_img_url} />
+        <p>Topic: {article.topic}</p>
+        <p>Comments: {article.comment_count}</p>
+        <p>Votes: {article.votes}</p>
+        <p>{article.body}</p>
+      </section>
+      <section className="comment-list">
+        <h4>Comments</h4>
+        <ul>
+          {comments.map((comment) => {
+            return <CommentCard key={comment.comment_id} comment={comment} />;
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
