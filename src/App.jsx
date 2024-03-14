f4d85489363f586db83951d9fd1060240b59d9b0
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-03-14 14:58:15 +0000

    add UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import ArticleList from "./components/ArticleList";
 import TopicList from "./components/TopicList";
 import SingleArticle from "./components/SingleArticle";
+import UserContext from "./contexts/User";
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({
+    username: "cooljmessy",
+    name: "Peter Messy",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
+  });
+
   return (
-    <div>
+    <UserContext.Provider value={{loggedInUser, setLoggedInUser}}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/articles" element={<ArticleList />} />
-        <Route path="/articles/:article_id" element={<SingleArticle />}/>
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
         <Route path="/topics" element={<TopicList />} />
       </Routes>
-    </div>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../contexts/User";
 
 function Header() {
+  const { loggedInUser } = useContext(UserContext);
   return (
     <header>
       <h1>NC News</h1>
+      <p>Welcome, {loggedInUser.username}!</p>
       <nav>
         <Link className="nav-link" to="/">Home</Link>
         <Link className="nav-link" to="/articles">Articles</Link>
