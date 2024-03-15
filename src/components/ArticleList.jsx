1bbfd123562c7d2b643c4deb934e3e3037c57045
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-03-15 14:17:59 +0000

    create dropdown for sorting

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -6,9 +6,10 @@ import ArticleCard from "./ArticleCard";
 function ArticleList() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sort, setSort] = useState('date');
   const [searchParams, setSearchParams] = useSearchParams();
   const topicQuery = searchParams.get("topic");
-
+console.log(sort)
   useEffect(() => {
     getArticles().then((response) => {
       const articles = response.data.articles;
@@ -28,6 +29,16 @@ function ArticleList() {
   return (
     <section>
       <h2>View all articles: </h2>
+      <div className="dropdown">
+        <label>
+          Sort articles by
+          <select value={sort} onChange={(event)=>{setSort(event.target.value)}}>
+            <option value="date">Date</option>
+            <option value="comment_count">Comment Count</option>
+            <option value="votes">Votes</option>
+          </select>
+        </label>
+      </div>
       <ul>
         {articles.map((article) => {
           return <ArticleCard key={article.article_id} article={article} />;
